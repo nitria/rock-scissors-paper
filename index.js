a0b4052c14ea509be97a689da4723b37fd09dbc9
Nick <41241228+nitria@users.noreply.github.com>
2020-12-11 12:33:20 +0200

    some javascript fixes

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-var rock = document.querySelector(".rock");
-var scissors = document.querySelector(".scissors");
-var paper = document.querySelector(".paper");
 var choices = ["Rock", "Scissors", "Paper"];
 var playerScore = 0;
-var aiScore = 0;
+var computerScore = 0;
 
 $(".rock").on("click", () => {
-  $("#player").html("Rock");
+  $("#playerChoice").html("Rock");
   generateRandomChoice();
   game();
+  gameOver();
 });
 
 $(".scissors").on("click", () => {
-  $("#player").html("Scissors");
+  $("#playerChoice").html("Scissors");
   generateRandomChoice();
   game();
+  gameOver();
 });
 
 $(".paper").on("click", () => {
-  $("#player").html("Paper");
+  $("#playerChoice").html("Paper");
   generateRandomChoice();
   game();
+  gameOver();
 });
 
 function game() {
-  switch ($("#player").html() + $("#ai").html()) {
+  switch ($("#playerChoice").html() + $("#computerChoice").html()) {
     case "RockScissors":
     case "PaperRock":
     case "ScissorsPaper":
@@ -34,36 +34,126 @@ function game() {
     case "ScissorsRock":
     case "RockPaper":
     case "PaperScissors":
-      $(".showresult").html("you lose malaka!");
+      $(".showresult").html("you lose");
       lose();
       break;
     case "RockRock":
     case "PaperPaper":
     case "ScissorsScissors":
       $(".showresult").html("draw");
+      draw();
       break;
   }
 }
 
 //Random choice for AI//
 function generateRandomChoice() {
-  $("#ai").html(choices[Math.floor(Math.random() * choices.length)]);
+  $("#computerChoice").html(
+    choices[Math.floor(Math.random() * choices.length)]
+  );
 }
 
 //Function update score//
+var previous = null;
+var previousText = null;
 function win() {
+  var currentText = $(".showresult").text();
+  if (previous == previousText) {
+    if (previousText == currentText) {
+      $(".showresult").html("you're on fire");
+    }
+  }
+  previous = previousText;
+  previousText = currentText;
+
   $(".playerscore").html((playerScore += 1));
 }
 
 function lose() {
-  $(".aiscore").html((aiScore += 1));
+  var currentText = $(".showresult").text();
+  if (previous == previousText) {
+    if (previousText == currentText) {
+      $(".showresult").html("go home malaka!");
+    }
+  }
+  previous = previousText;
+  previousText = currentText;
+
+  $(".computerscore").html((computerScore += 1));
 }
 
+function draw() {
+  var currentText = $(".showresult").text();
+  if (previous == previousText) {
+    if (previousText == currentText) {
+      $(".showresult").html("it's a draw again");
+    }
+  }
+  previous = previousText;
+  previousText = currentText;
+}
+
+//Game over screen//
+function gameOver() {
+  var overlay;
+  var restartGame;
+  var text;
+  if (playerScore == 10 || computerScore == 10) {
+    overlay = $("<div class='overlay'></div>");
+    overlay.css({
+      width: "100%",
+      height: "100%",
+      position: "absolute",
+      top: "0",
+      left: "0",
+      background: "rgba(0,0,0,0.85)",
+      display: "flex",
+      "align-items": "center",
+      "justify-content": "space-evenly",
+      "flex-direction": "column",
+    });
+
+    text = $("<h1>Game Over</h1>");
+    text.css({
+      "font-size": "5.5em",
+      color: "#fff",
+    });
+
+    restartGame = $("<button id='restartGame'>Restart Game</button>");
+    restartGame.css({
+      background: " #008ecc",
+      color: "#fff",
+      padding: "10px 20px",
+      "font-size": "1.5em",
+      border: "none",
+      "border-radius": "10px",
+      cursor: "pointer",
+    });
+    $("body").append(overlay);
+    $(".overlay").append(text);
+    $(".overlay").append(restartGame);
+  }
+
+  //Restart Game//
+  $("#restartGame").on("click", () => {
+    playerScore = 0;
+    computerScore = 0;
+    $("#playername").html("You:");
+    $("#playerChoice").html("");
+    $("#computerChoice").html("");
+    $(".playerscore").html(playerScore);
+    $(".computerscore").html(computerScore);
+    $(".overlay").remove();
+    $(".showresult").html("");
+  });
+}
+
+//Button to enter your name//
 $("#btn").on("click", () => {
   var playerName = $("input").val();
-  $(".playerdiv")
-    .children("h1")
-    .html(playerName)
+  $("h1")
+    .children("#playername")
+    .html(playerName + ":")
     .css("text-transform", "capitalize");
   $("input").val("");
 });
